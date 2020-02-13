744e3d5edf6705f4fee67d0d8df73bc4c50ec079
arjbhavs <arjbhavs@iu.edu>
2020-02-13 03:44:05 +0000

    Validations Added

--- shell/xsh_run.c
+++ shell/xsh_run.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <prodcons_bb.h>
+#include <ctype.h>
 /*------------------------------------------------------------------------
  * xsh_run - obtain and print the current month, day, year, and time
  *------------------------------------------------------------------------
@@ -48,29 +49,56 @@ shellcmd xsh_run(int nargs, char *args[]) {
 
 
 if ((nargs == 1) || (strncmp(args[1], "list", 5) == 0)) {
-      printf("my_function_1\n");
-      printf("my_function_2\n");
+      printf("prodcons_bb\n");
+     // printf("my_function_2\n");
       return OK;
     }
+     if (nargs == 2 && strncmp(args[1], "--help", 7) == 0) {
+		printf("Usage: %s\n\n", args[0]);
+		printf("Description:\n");
+		printf("\tRun Command will Allow to to execute multiple commands such as Proodcons_bb \n");
+		printf("Options (one per invocation):\n");
+		printf("\t--help\tdisplay this help and exit\n");
+		return 0;
+	}
 
-    /* This will go past "run" and pass the function/process name and its
-    * arguments.
-    */
-    args++;
-    nargs--;
+	/* Check argument count */
+
+	if (nargs > 6) {
+		fprintf(stderr, "%s: too many arguments\n", args[0]);
+		fprintf(stderr, "Try '%s --help' for more information\n",
+			args[0]);
+		return 1;
+	}
+	if(nargs<6 ){
+		fprintf(stderr, "%s: too less arguments\n", args[0]);
+		fprintf(stderr, "Try '%s --help' for more information\n",
+			args[0]);
+		return 1;
+	}
+	
+   // if (isdigit(args[2]) == isdigit(args[3])== isdigit(args[4]) == isdigit(args[5]) == 0){
+	    /* This will go past "run" and pass the function/process name and its
+   	 * arguments.
+   	 */
+   	 args++;
+   	 nargs--;
     
-    int numProducerPro = atoi(args[1]);
-    int numConsumerPro = atoi(args[2]);
-    int producerCount = atoi(args[3]);
-    int consumerCount = atoi(args[4]);
+   	 int numProducerPro = atoi(args[1]);
+   	 int numConsumerPro = atoi(args[2]);
+   	 int producerCount = atoi(args[3]);
+   	 int consumerCount = atoi(args[4]);
 
-    if((numProducerPro * producerCount) == (numConsumerPro * consumerCount)){
+    	if((numProducerPro * producerCount) == (numConsumerPro * consumerCount)){
 
-    	if(strncmp(args[0], "prodcons_bb", 13) == 0) {
-      	/* create a process with the function as an entry point. */
-      	resume (create((void *)prodcons_bb, 4096, 20, "prodcons_bb", 2, nargs, args));
-    	}
-    }else{
-	printf("Mismatch in iteration count of producer and consumer");
-    }
+ 	   	if(strncmp(args[0], "prodcons_bb", 13) == 0) {
+      			/* create a process with the function as an entry point. */
+	      		resume (create((void *)prodcons_bb, 4096, 20, "prodcons_bb", 2, nargs, args));
+    		}
+   	 }else{
+		printf("Mismatch in iteration count of producer and consumer");
+   	 }
+//  }else{
+ //	 printf("Enter Valid Arguements.");
+ // }
 }
